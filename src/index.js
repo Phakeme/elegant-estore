9e9ebb53001123796e693f45383de925f8268f4c
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-07-04 06:29:51 +0200

    Added Material-IU

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#fff'
+    },
+    secondary: {
+      main: '#f68b1e'
+  },
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
