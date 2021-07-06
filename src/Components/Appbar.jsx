c907b4c42f795dd321e7eb5bf1e7ee2ef36b0b9c
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-07-06 03:59:28 +0200

    Added Search form to the Appbar

--- src/Components/Appbar.jsx
+++ src/Components/Appbar.jsx
@@ -7,11 +7,27 @@ import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
 
 const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
   appbar: {
     minHeight: '76px',
-    background: 'white',
+    display: 'flex',
+    justifyContent: 'center'
   },
   toolbar: {
     height: '32px',
@@ -26,14 +42,46 @@ const useStyles = makeStyles(theme => ({
   navlogo: {
     height: '100%',
   },
-  search: {
-    // background: 'blue',
+  searchBox: {
+    // background: 'grey',
     maxWidth: '660px',
-    border: '1px solid grey',
-    borderRadius: '3px',
+    // border: '1px solid grey',
+    // borderRadius: '3px',
     width: '100%',
+    minWidth:"200px",
     height: '40px',
     marginRight: '16px'
+  },
+  search: {
+    position: 'relative',
+    border: '1px solid #c8c8c8',
+    borderRadius: '3px',
+    backgroundColor: fade('#fff', 0.15),
+    '&:hover': {
+      backgroundColor: fade('#fff', 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+  },
+  searchIcon: {
+    padding: '1px 1px 1px 10px',
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%'
+  },
+  inputInput: {
+    padding: '10px',
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${30}px)`,
+    transition: '',
+    width: '100%',
   },
   info: {
     minWidth: '200px'
@@ -44,7 +92,7 @@ export default function PrimarySearchAppBar() {
   const classes = useStyles();
 
   return (
-    <AppBar elevation={1}>
+    <AppBar elevation={1} className={classes.appbar}>
       <Container>
         <Toolbar className={classes.toolbar}>
             <Grid item className={classes.navbrand}>
@@ -52,9 +100,23 @@ export default function PrimarySearchAppBar() {
               <img className={classes.navlogo} src='./images/elegant-logo.svg' alt="brand-logo" />
               </Link>
               </Grid>
-            <Grid item  className={classes.search}>
-              <Typography  variant="h6"></Typography>
+            <Grid item  className={classes.searchBox}>
+             
+           <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search products, brands and categories"
+              inputProps={{ 'aria-label': 'Search products, brands and categories' }}
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+            />
+          </div>
             </Grid>
+
             <Box flexGrow={1}>
               <Typography  variant="h6" className={classes.info}>Elegant 3</Typography>
             </Box>
