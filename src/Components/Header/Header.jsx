12ebfbfdef3efd2436a032644a789e5d2f6ced96
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-07-08 05:07:01 +0200

    Added Admin Section on the Main Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,10 +9,14 @@ import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import SearchIcon from '@material-ui/icons/Search';
 import InputBase from '@material-ui/core/InputBase';
-import {FaRegUser} from 'react-icons/fa'
+import {FaRegUser, FaRegQuestionCircle} from 'react-icons/fa'
 import {BiChevronDown} from 'react-icons/bi'
+import {GrCart} from 'react-icons/gr/'
+
 // FaRegUser
 // BiChevronDown
+// FaRegQuestionCircle
+// GrCart
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -120,27 +124,33 @@ export default function Header() {
             />
           </div>
             </Grid>
-
             <Box flexGrow={1} style={{
-                // backgroundColor:'red',
-                height: 39
+                height: 39,
+                minWidth: 286
                 }}>
               <Grid container style={{
                 height: '100%',
                 alignItems:'center',
-                textAlign: 'center'
+                textAlign: 'center',
                 }}>
                   <Grid item xs={4} 
                   style={{backgroundColor:''}}>
                     <label>
                       <span><FaRegUser fontSize={20} style={{marginRight:5}}/> Login <BiChevronDown/></span>
-                     
                     </label>
                   </Grid>
                   <Grid item xs={4}
-                  style={{backgroundColor:'yellow'}}>Elegant 2</Grid>
+                  style={{backgroundColor:''}}>
+                    <label>
+                      <span><FaRegQuestionCircle fontSize={20} style={{marginRight:5}}/> Help <BiChevronDown/></span>
+                    </label>
+                  </Grid>
                   <Grid item xs={4}
-                  style={{backgroundColor:'green'}}>Elegant 3</Grid>
+                  style={{backgroundColor:'#f5f5f5',borderRadius:4, height: '100%',  display: 'grid',alignItems: 'center'}}>
+                    <label>
+                      <span><GrCart fontSize={20}/> Cart</span>
+                    </label>
+                  </Grid>
               </Grid>
             </Box>
           </Toolbar>
