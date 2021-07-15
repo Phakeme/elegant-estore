243bda79b5e7fa3442232cbe04e25145a39898ca
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-07-15 11:04:26 +0200

    Updated Single Product Page

--- src/Components/Product/index.js
+++ src/Components/Product/index.js
@@ -1,31 +1,68 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { commerce } from "../../lib/commerce";
 import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  navigation: {
+    marginRight: "8px",
+  },
+  mainPaper: {
+    height: "100%",
+  },
+}));
 
 const Product = () => {
+  const classes = useStyles();
   const [product, setProduct] = useState({});
   const { id } = useParams();
 
   useEffect(() => {
-    commerce.products
-      .retrieve(id)
-      .then((product) => setProduct(product), console.log(product, "Product"));
+    commerce.products.retrieve(id).then((product) => setProduct(product));
   }, []);
 
   return (
-    <Container style={{ marginTop: `${76 + 8 + 8}px` }}>
-      <h1>Product</h1>
-      {product.hasOwnProperty("name") && (
-        <div>
-          <div>{product.name}</div>
+    <Container style={{ marginTop: `${76 + 8}px` }}>
+      {console.log(product, "Product")}
+      <Box style={{ marginBottom: "8px" }}>
+        <Grid container>
+          <Grid item>
+            <Link to="/" className={classes.navigation}>
+              Home
+            </Link>
+          </Grid>
+          {Array.isArray(product.categories) &&
+            product.categories.map(({ id, name }, i) => (
+              <Grid item key={id}>
+                <span> {`${" > "}`} </span>
+                <Link to={`/`} className={classes.navigation}>{`${name}`}</Link>
+              </Grid>
+            ))}
+        </Grid>
+      </Box>
+      <Grid container spacing={2} style={{ minHeight: "400px" }}>
+        <Grid item xs={9}>
+          <Paper className={classes.mainPaper}>
+            {product.hasOwnProperty("name") && (
+              <div>
+                {/* <div>{product.name}</div>
           <div>{product.price.formatted_with_symbol}</div>
           <div>{product.description}</div>
           {product.assets.map((image, i) => (
             <img key={i} src={image.url} alt="product" />
-          ))}
-        </div>
-      )}
+          ))} */}
+              </div>
+            )}
+          </Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.mainPaper}></Paper>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
