21c88545b4ab0628e0ecf0905c37111e8469cd36
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-08-01 12:18:47 +0200

    Checkout: Added Textfields

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Box, Typography, Paper, FormGroup } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
+import { HiCheckCircle } from "react-icons/hi";
 import { makeStyles } from "@material-ui/styles";
 
 const Styles = makeStyles((theme) => ({
@@ -14,22 +16,6 @@ const Styles = makeStyles((theme) => ({
     maxWidth: "100%",
     margin: "auto",
   },
-  header: {
-    height: "30px",
-    textTransform: "uppercase",
-  },
-  items: {
-    width: "53.47%",
-  },
-  quantity: {
-    width: "10.315%",
-  },
-  unitPrice: {
-    width: "18.105%",
-  },
-  subTotal: {
-    width: "18.105%",
-  },
   actionBtns: {
     display: "flex",
     justifyContent: "flex-end",
@@ -39,30 +25,50 @@ const Styles = makeStyles((theme) => ({
     color: theme.palette.primary.main,
     width: 250,
   },
+  icon: {
+    fontSize: "1.5rem",
+    color: theme.palette.secondary.main,
+  },
 }));
 
 const Checkout = () => {
   const classes = Styles();
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={8}>
-        <Typography variant="h5" paragraph component="h2">
-          <Box>Checkout</Box>
-        </Typography>
-        <Paper style={{ padding: 10 }}>
-          <div>Checkout</div>
-        </Paper>
+    <FormGroup>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Typography variant="h5" paragraph component="h2">
+            <Box>Checkout</Box>
+          </Typography>
+          <Paper style={{ padding: 10 }}>
+            <Grid container>
+              <Grid item xs={1}>
+                <HiCheckCircle className={classes.icon} />
+              </Grid>
+              <Grid item xs={11}>
+                <form noValidate autoComplete="off">
+                  <TextField
+                    id="standard-basic"
+                    color="secondary"
+                    fullWidth
+                    label="Name"
+                  />
+                </form>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="h5" paragraph component="h2">
+            <Box>Order Summary</Box>
+          </Typography>
+          <Paper style={{ padding: 10 }}>
+            <div>Checkout</div>
+          </Paper>
+        </Grid>
       </Grid>
-      <Grid item xs={4}>
-        <Typography variant="h5" paragraph component="h2">
-          <Box>Order Summary</Box>
-        </Typography>
-        <Paper style={{ padding: 10 }}>
-          <div>Checkout</div>
-        </Paper>
-      </Grid>
-    </Grid>
+    </FormGroup>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+// import { IconContext } from "react-icons";
 
 const theme = createMuiTheme({
   palette: {
@@ -19,7 +20,9 @@ const theme = createMuiTheme({
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      {/* <IconContext.Provider value={{ size: "1rem" }}> */}
       <App />
+      {/* </IconContext.Provider> */}
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
