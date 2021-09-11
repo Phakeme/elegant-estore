35b5b9ef331e1fdb97b9a3e3bcfa48ce900b978f
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-09-11 08:27:25 +0200

    centralized <Progress/> default export

--- src/Components/index.js
+++ src/Components/index.js
@@ -16,4 +16,5 @@ export { default as Product } from "./Product";
 export { default as Top4Links } from "./Top4Links/";
 export { default as Footer } from "./Footer/";
 export { default as ScrollToTop } from "./utils/ScrollToTop";
+export { default as Progress } from "./utils/Progress";
 export { default as AddToCartModal } from "./utils/AddToCartModal";
