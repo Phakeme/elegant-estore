11c4263f702fd1e264f96e8b26ae69a8f0512cd1
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-09-15 22:29:54 +0200

    Banner: Removed useState unused import

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Paper from "@material-ui/core/Paper";
