7790e600b217dca4f30a2091cbec7edb59442cd0
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-09-17 20:13:04 +0200

    SPHelp: Added SPHelpContainer

---
+++ src/containers/Pages/SPHelpContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SPHelp, GlobalContainer } from "../../Components";
+
+const QueryContainer = ({ sortedProducts, query }) => {
+  return (
+    <GlobalContainer>
+      <SPHelp></SPHelp>
+    </GlobalContainer>
+  );
+};
+
+export default QueryContainer;
