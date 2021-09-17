9270bc5aae64d7575ecc962678c8c6c70d1251e9
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-09-17 20:18:49 +0200

    Added SPHelp Component

---
+++ src/Components/SPHelp/SPHelp.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Paper from "@material-ui/core/Paper";
+import Card from "@material-ui/core/Card";
+import Divider from "@material-ui/core/Divider";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardMedia from "@material-ui/core/CardMedia";
+import Styles from "./styles";
+
+const SPHelp = () => {
+  const classes = Styles();
+  return (
+    <Paper className={classes.paper}>
+      <div className={classes.banner}></div>
+      <Grid container spacing={2} style={{ padding: 10 }}>
+        <Grid item xs={8} style={{ marginTop: "-80px" }}>
+          <div className={classes.gridArea}>
+            <div className={classes.iconLink}>Text</div>
+            <div className={classes.iconLink}>Text</div>
+            <div className={classes.iconLink}>Text</div>
+            <div className={classes.iconLink}>Text</div>
+            <div className={classes.iconLink}>Text</div>
+            <div className={classes.iconLink}>Text</div>
+          </div>
+        </Grid>
+        <Grid item xs={4}>
+          Text
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
+
+export default SPHelp;
