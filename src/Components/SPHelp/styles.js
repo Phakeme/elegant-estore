6861890a93e0f28cdcaf179b9aaffd39c1bf2f59
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-09-18 12:19:43 +0200

    Updated Styles.js

--- src/Components/SPHelp/styles.js
+++ src/Components/SPHelp/styles.js
@@ -1,4 +1,5 @@
 import { makeStyles } from "@material-ui/styles";
+import logo from "../../img/helpBanner.svg";
 
 export default makeStyles((theme) => ({
   paper: {
@@ -9,18 +10,40 @@ export default makeStyles((theme) => ({
     padding: "10px",
   },
   banner: {
-    background: theme.palette.secondary.main,
+    backgroundImage: `url(${logo})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "noRepeat",
+    backgroundPosition: "center",
     height: 226,
     "@media (max-width: 1280px)": {},
   },
   iconLink: {
-    height: 257.8,
-    // width: "100%",
+    height: 220,
+    maxWidth: 257.78,
+    width: "100%",
+    borderRadius: 5,
+    padding: 10,
+    display: "flex",
+    alignItems: "center",
+    textAlign: "center",
+    justifyContent: "center",
     background: theme.palette.grey[100],
   },
   gridArea: {
     display: "grid",
-    gap: 8,
-    gridTemplateAreas: '"z z z" "a a a" ',
+    gap: 16,
+    width: "100%",
+    gridTemplateColumns: "1fr 1fr 1fr",
+  },
+  icon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 16,
+    fontSize: "4rem",
+  },
+  label: {
+    fontWeight: 700,
+    fontSize: 16,
   },
 }));
