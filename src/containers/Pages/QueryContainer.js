7211e8a4b30a77448bcb213713c75fd850eef595
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-09-19 10:29:39 +0200

    QueryContainer: Added searchProducts Props

--- src/containers/Pages/QueryContainer.js
+++ src/containers/Pages/QueryContainer.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Banner, CurrentPath, Query, GlobalContainer } from "../../Components";
 
-const QueryContainer = ({ sortedProducts, query }) => {
+const QueryContainer = ({ sortedProducts, query, searchProducts }) => {
   return (
     <GlobalContainer>
-      <Query sortedProducts={sortedProducts} query={query}>
+      <Query
+        sortedProducts={sortedProducts}
+        query={query}
+        searchProducts={searchProducts}
+      >
         <CurrentPath />
         <Banner numOfProds={50} products={sortedProducts} />
       </Query>
