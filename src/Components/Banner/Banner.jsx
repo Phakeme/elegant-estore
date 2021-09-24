6b79ac381a6f33acd347d204f073c4b92446532a
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-09-24 07:50:52 +0200

    Banner: Updated Link to url

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -37,7 +37,7 @@ const Banner = ({ products, title, numOfProds, xs, getProduct }) => {
               <Grid item xs={xs} key={i}>
                 <Link
                   key={i}
-                  to={`product/${product.name.split(" ").join("-")}/${
+                  to={`/product/${product.name.split(" ").join("-")}/${
                     product.id
                   }`}
                 >
