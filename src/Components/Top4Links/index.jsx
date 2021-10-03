4086e9ba91df94a87d7cd72c9eb94917c75f7f2f
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-10-03 11:50:00 +0200

    Top4Links: Updated Link to path

--- src/Components/Top4Links/index.jsx
+++ src/Components/Top4Links/index.jsx
@@ -3,12 +3,18 @@ import Grid from "@material-ui/core/Grid";
 import QuickLink from "./QuickLink";
 import quicklinks from "../../Data/quickLinks";
 
-const QuickLinks = () => {
+const QuickLinks = ({ searchProducts }) => {
   return (
     <section>
       <Grid container spacing={2}>
         {quicklinks.map(({ title, icon, path }, index) => (
-          <QuickLink key={index} title={title} icon={icon} path={path} />
+          <QuickLink
+            key={index}
+            title={title}
+            icon={icon}
+            path={path}
+            searchProducts={searchProducts}
+          />
         ))}
       </Grid>
     </section>
