f931d26b2a743d11de825fbb66e668fd75035818
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-10-06 20:09:20 +0200

    Top4Links: Updated media queries

--- src/Components/Top4Links/QuickLink.jsx
+++ src/Components/Top4Links/QuickLink.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
-// import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core";
 
 const QuickLink = ({ icon, title, path, searchProducts }) => {
   const Styles = makeStyles((theme) => ({
     root: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
       height: 56,
       padding: 5,
       marginBottom: 16,
+      "@media (max-width: 960px)": {
+        // background: "red",
+        padding: "0 8px",
+      },
     },
     icon: {
       display: "flex",
@@ -18,13 +24,14 @@ const QuickLink = ({ icon, title, path, searchProducts }) => {
       alignItems: "center",
     },
     title: {},
+    item: { background: "" },
   }));
   const classes = Styles();
   return (
-    <Grid item xs={3} onClick={() => searchProducts(path)}>
+    <Grid item xs={6} md={3} onClick={() => searchProducts(path)}>
       <Link to={`/search/${path}`}>
         <Paper className={classes.root}>
-          <Grid container alignItems="center">
+          <Grid container alignItems="center" className={classes.item}>
             <Grid item xs={2} className={classes.icon}>
               <img src={icon} alt="" style={{ height: "40px" }} />
             </Grid>
