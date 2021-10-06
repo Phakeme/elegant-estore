e0b6aa592da49cf58ddb3412170f5415be65fe93
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-10-06 20:09:42 +0200

    Top4Links: Updated media queries

--- src/Components/Top4Links/index.jsx
+++ src/Components/Top4Links/index.jsx
@@ -3,10 +3,23 @@ import Grid from "@material-ui/core/Grid";
 import QuickLink from "./QuickLink";
 import quicklinks from "../../Data/quickLinks";
 
+import { makeStyles } from "@material-ui/core";
+
+const Styles = makeStyles((theme) => ({
+  root: {
+    "@media (max-width: 960px)": {
+      margin: "0px 0px 0px -8px",
+    },
+  },
+  icon: {},
+  title: {},
+}));
+
 const QuickLinks = ({ searchProducts }) => {
+  const classes = Styles();
   return (
     <section>
-      <Grid container spacing={2}>
+      <Grid container spacing={2} className={classes.root}>
         {quicklinks.map(({ title, icon, path }, index) => (
           <QuickLink
             key={index}
