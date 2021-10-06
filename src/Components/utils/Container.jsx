599b970e550cea77ef8cb4c74d071d63cafe727b
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-10-06 20:06:22 +0200

    Container: removed fixed width, added 100% width

--- src/Components/utils/Container.jsx
+++ src/Components/utils/Container.jsx
@@ -4,9 +4,9 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const Styles = makeStyles((theme) => ({
   root: {
-    width: "1280px",
+    maxWidth: "1280px",
     "@media (max-width: 1280px)": {
-      width: "1040px",
+      maxWidth: "1040px",
       margin: "auto",
     },
   },
