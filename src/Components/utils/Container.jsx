59cc1e0dec998142f175b82a0fc3b47ae3988220
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-10-07 21:05:59 +0200

    GlobalContainer: updated breakpoints to match with MUIs

--- src/Components/utils/Container.jsx
+++ src/Components/utils/Container.jsx
@@ -5,7 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const Styles = makeStyles((theme) => ({
   root: {
     maxWidth: "1280px",
-    "@media (max-width: 1280px)": {
+    "@media (max-width: 1200px)": {
       maxWidth: "1040px",
       margin: "auto",
     },
