530fc1094f88c5b3873aea2efae59401025f42c9
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-10-08 22:07:23 +0200

    Header: added responsive styles

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -13,24 +13,29 @@ export default function Header({ cart, searchProducts, loading }) {
   const classes = useStyles();
   return (
     <AppBar
+      className={classes.appBar}
       elevation={1}
-      style={{ minHeight: "76px", display: "flex", justifyContent: "center" }}
+      style={{ display: "flex", justifyContent: "center" }}
     >
       {loading && <LinearProgress color="secondary" />}
       <GlobalContainer>
-        <Toolbar disableGutters>
+        <Toolbar disableGutters className={classes.toolBar}>
           <Grid container spacing={2}>
-            <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Grid item xs={7} sm={6} md={2} lg={2}>
               <Branding />
             </Grid>
             <Grid item sm={6} md={6} lg={7} className={classes.search}>
               <Search searchProducts={searchProducts} />
             </Grid>
-            <Grid item xs={6} sm={6} md={4} lg={3}>
+            <Grid item xs={5} sm={6} md={4} lg={3}>
               <Admin cart={cart} />
             </Grid>
           </Grid>
         </Toolbar>
+        <div className={classes.mobileSearch}>
+          <Search searchProducts={searchProducts} />
+          {/* Test */}
+        </div>
       </GlobalContainer>
     </AppBar>
   );
