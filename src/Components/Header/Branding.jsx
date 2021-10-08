f094424020cc1e07f6380b7dc539f46d91c1f820
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-10-08 22:04:02 +0200

    Branding Header updated

--- src/Components/Header/Branding.jsx
+++ src/Components/Header/Branding.jsx
@@ -1,17 +1,44 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
 import logo from "../../img/elegant-logo.svg";
 
+const styles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    height: "40px",
+    alignItems: "center",
+  },
+  logo: {
+    height: "30px",
+    "@media (max-width: 600px)": {
+      height: "25px",
+    },
+  },
+  brandBurger: {
+    fontSize: 25,
+    marginRight: 20,
+    "@media (min-width: 961px)": {
+      display: "none",
+    },
+  },
+  textLabel: {
+    "@media (max-width: 600px)": {
+      display: "none",
+    },
+  },
+}));
+
 const Branding = () => {
+  const classes = styles();
   return (
-    <div
-      style={{
-        display: "flex",
-        height: "40px",
-      }}
-    >
+    <div className={classes.root}>
+      <div>
+        <GiHamburgerMenu className={classes.brandBurger} />
+      </div>
       <Link to="/">
-        <img style={{ height: "80%" }} src={logo} alt="brand-logo" />
+        <img className={classes.logo} src={logo} alt="brand-logo" />
       </Link>
     </div>
   );
