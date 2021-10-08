f58024fae5eeeb03aa586c219e5bd70202b3e028
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-10-08 22:07:48 +0200

    Header: added responsive styles

--- src/Components/Header/styles.js
+++ src/Components/Header/styles.js
@@ -1,4 +1,5 @@
 import { makeStyles } from "@material-ui/styles";
+import { fade } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
   searchIcon: {
@@ -26,6 +27,42 @@ export default makeStyles((theme) => ({
   search: {
     "@media (max-width: 959px)": {
       display: "none",
+    },
+  },
+  appBar: {
+    "@media (max-width: 959px)": {
+      height: "110px",
+    },
+  },
+  toolBar: {
+    "@media (max-width: 959px)": {
+      minHeight: "20px",
+      marginBottom: 5,
+    },
+  },
+
+  mobileSearch: {
+    display: "none",
+    "@media (max-width: 959px)": {
+      width: "100%",
+      display: "block",
+    },
+  },
+
+  searchBtn: {
+    "@media (max-width: 600px)": {
+      display: "none",
+    },
+  },
+  searchBox: {
+    position: "relative",
+    border: "1px solid #c8c8c8",
+    borderRadius: "3px",
+    backgroundColor: fade("#fff", 0.15),
+    marginRight: 10,
+    height: "40px",
+    "@media (max-width: 600px)": {
+      marginRight: 0,
     },
   },
 }));
