e17a1f2186682df361bd51804e06c54c7f915911
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-10-10 22:05:39 +0200

    src/Components/Banner/Thumbnails.jsx

--- src/Components/Banner/Thumbnails.jsx
+++ src/Components/Banner/Thumbnails.jsx
@@ -2,35 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import Styles from "./styles";
 
 const Thumbnails = ({ thumbnails, searchProducts }) => {
+  const classes = Styles();
   return (
-    <Paper style={{ height: 173, padding: 15, marginBottom: 16 }}>
-      <Grid container spacing={0} style={{ height: "100%" }}>
-        {thumbnails.map(({ icon, path, title }, index) => (
-          <Grid key={index} item xs={2} onClick={() => searchProducts(path)}>
-            <Link to={`/search/${path}`}>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                  height: "100%",
-                }}
-              >
-                <img
-                  style={{ marginBottom: 10, width: "100%" }}
-                  src={icon}
-                  alt=""
-                />
-                <span>{title}</span>
-              </div>
-            </Link>
-          </Grid>
-        ))}
-      </Grid>
-    </Paper>
+    <div className={classes.fluidBox}>
+      <Paper style={{ height: 173, padding: 15, marginBottom: 16 }}>
+        <Grid container spacing={0} style={{ height: "100%" }}>
+          {thumbnails.map(({ icon, path, title }, index) => (
+            <Grid key={index} item xs={2} onClick={() => searchProducts(path)}>
+              <Link to={`/search/${path}`}>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    height: "100%",
+                  }}
+                >
+                  <img
+                    style={{ marginBottom: 10, width: "100%" }}
+                    src={icon}
+                    alt=""
+                  />
+                  <span>{title}</span>
+                </div>
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+      </Paper>
+    </div>
   );
 };
 
