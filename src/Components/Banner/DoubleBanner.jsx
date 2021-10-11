09a642f243e99f2151bd8114efba885200657632
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-10-11 20:14:52 +0200

    DoubleBanner Styles: Responsive width updated

--- src/Components/Banner/DoubleBanner.jsx
+++ src/Components/Banner/DoubleBanner.jsx
@@ -2,43 +2,43 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Styles from "./styles";
 
 const DoubleBanner = ({ searchProducts }) => {
   const classes = Styles();
+  const theme = useTheme();
+  const isSmall = useMediaQuery(theme.breakpoints.down("xs"));
   return (
-    <div className={classes.fluidBox}>
-      <Paper className={classes.paper}>
-        <Grid container spacing={0}>
-          <Grid item xs={6} onClick={() => searchProducts("women")}>
-            <Link to="/search/women">
-              <div
-                style={{
-                  height: 258,
-                  backgroundImage:
-                    "url(./images/doubleBanner/DT-Double-banner-Women.jpg)",
-                  backgroundPosition: "center",
-                  backgroundRepeat: "no-repeat",
-                }}
-              ></div>
-            </Link>
-          </Grid>
-          <Grid item xs={6} onClick={() => searchProducts("men")}>
-            <Link to="/search/men">
-              <div
-                style={{
-                  height: 258,
-                  backgroundImage:
-                    "url(./images/doubleBanner/DT-Double-banner-Men.jpg)",
-                  backgroundPosition: "center",
-                  backgroundRepeat: "no-repeat",
-                }}
-              ></div>
-            </Link>
-          </Grid>
-        </Grid>
-      </Paper>
-    </div>
+    <Grid container spacing={isSmall ? 0 : 2}>
+      <Grid item xs={12} sm={6} onClick={() => searchProducts("women")}>
+        <Link to="/search/women">
+          <Paper className={classes.DoubleBannerpaper}>
+            <div>
+              <img
+                style={{ width: "100%" }}
+                src="./images/doubleBanner/DT-Double-banner-Women.jpg"
+                alt="doubleBanner Women"
+              />
+            </div>
+          </Paper>
+        </Link>
+      </Grid>
+      <Grid item xs={12} sm={6} onClick={() => searchProducts("men")}>
+        <Link to="/search/men">
+          <Paper className={classes.DoubleBannerpaper}>
+            <div>
+              <img
+                style={{ width: "100%" }}
+                src="./images/doubleBanner/DT-Double-banner-Men.jpg"
+                alt="doubleBanner men"
+              />
+            </div>
+          </Paper>
+        </Link>
+      </Grid>
+    </Grid>
   );
 };
 
