3812477e71c27aa5608049167174866602423317
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-10-15 20:23:34 +0200

    Header: Added MUI Drawer

--- src/Components/Header/Branding.jsx
+++ src/Components/Header/Branding.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
 import logo from "../../img/elegant-logo.svg";
+import Drawer from "./Drawer";
 
 const styles = makeStyles((theme) => ({
   root: {
@@ -14,13 +14,6 @@ const styles = makeStyles((theme) => ({
     height: "30px",
     "@media (max-width: 600px)": {
       height: "25px",
-    },
-  },
-  brandBurger: {
-    fontSize: 25,
-    marginRight: 20,
-    "@media (min-width: 961px)": {
-      display: "none",
     },
   },
   textLabel: {
@@ -35,7 +28,7 @@ const Branding = () => {
   return (
     <div className={classes.root}>
       <div>
-        <GiHamburgerMenu className={classes.brandBurger} />
+        <Drawer />
       </div>
       <Link to="/">
         <img className={classes.logo} src={logo} alt="brand-logo" />
