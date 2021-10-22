1fb8b78f8d7ea569830967461339fa6c99b1b44d
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-10-22 19:26:57 +0200

    Styles: Added Mobile Styles

--- src/Components/Checkout/styles.js
+++ src/Components/Checkout/styles.js
@@ -9,15 +9,15 @@ export default makeStyles((theme) => ({
     maxWidth: "100%",
     margin: "auto",
   },
-  actionBtns: {
-    display: "flex",
-    justifyContent: "flex-end",
-  },
-  actionBtn: {
-    marginLeft: 10,
-    color: theme.palette.primary.main,
-    width: 250,
-  },
+  // actionBtns: {
+  //   display: "flex",
+  //   justifyContent: "flex-end",
+  // },
+  // actionBtn: {
+  //   marginLeft: 10,
+  //   color: theme.palette.primary.main,
+  //   width: 250,
+  // },
   icon: {
     fontSize: "1.5rem",
     color: theme.palette.secondary.main,
@@ -32,9 +32,12 @@ export default makeStyles((theme) => ({
   stepper: {
     marginBottom: 16,
     height: 10,
+    "@media (max-width: 960px)":{
+      display: "none"
+    }
   },
   btnNext: {
-    margin: "16px 0",
+    marginTop: "16px",
     color: theme.palette.primary.main,
   },
   paymentResults: {
