4787a0a5d9aa222606b4d30cb11b7ad8844924e8
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-10-22 19:25:31 +0200

    Review: Added Mobile Review

--- src/Components/Checkout/Review.jsx
+++ src/Components/Checkout/Review.jsx
@@ -12,41 +12,41 @@ const OrderSummary = ({ checkoutData }) => {
 
   if (!checkoutData) checkoutData = JSON.parse(localData);
 
-  return (
-    checkoutData ?
+  return checkoutData ? (
     <Paper style={{ padding: 10 }}>
-    <Box style={{ padding: 0 }}>
-      <div style={{ marginBottom: 10 }}>
-        <span style={{ fontSize: 14, fontWeight: 500 }}>ORDER SUMMARY</span>
-      </div>
-      <Divider />
-      <List disablePadding>
-        {checkoutData &&
-          checkoutData.live.line_items.map((product, index) => (
-            <ListItem style={{ padding: "10px 0" }} key={index}>
-              <div>
-                <img
-                  style={{ height: 80, marginRight: 10 }}
-                  src={product.media.source}
-                  alt=""
-                />
-              </div>
-              <ListItemText
-                primary={product.name}
-                secondary={`Qty:${product.quantity}, Size: ${product.selected_options[0].option_name}, Price: ${product.price.formatted_with_symbol}`}
-              />
-            </ListItem>
-          ))}
+      <Box>
+        <div style={{ marginBottom: 10 }}>
+          <span style={{ fontSize: 14, fontWeight: 500 }}>ORDER SUMMARY</span>
+        </div>
         <Divider />
-        <ListItem style={{ padding: " 0 10px" }}>
-          <ListItemText primary="Total:" />
-          <Typography variant="subtitle1" style={{ fontWeight: 700 }}>
-            {checkoutData && checkoutData.live.total.formatted_with_symbol}
-          </Typography>
-        </ListItem>
-      </List>
-    </Box> </Paper>: null
-  );
+        <List disablePadding>
+          {checkoutData &&
+            checkoutData.live.line_items.map((product, index) => (
+              <ListItem style={{ padding: "10px 0" }} key={index}>
+                <div>
+                  <img
+                    style={{ height: 50, marginRight: 10 }}
+                    src={product.media.source}
+                    alt=""
+                  />
+                </div>
+                <ListItemText
+                  primary={product.name}
+                  secondary={`Qty:${product.quantity}, Size: ${product.selected_options[0].option_name}, Price: ${product.price.formatted_with_symbol}`}
+                />
+              </ListItem>
+            ))}
+          <Divider />
+          <ListItem style={{ padding: " 0 10px" }}>
+            <ListItemText primary="Total:" />
+            <Typography variant="subtitle1" style={{ fontWeight: 700 }}>
+              {checkoutData && checkoutData.live.total.formatted_with_symbol}
+            </Typography>
+          </ListItem>
+        </List>
+      </Box>{" "}
+    </Paper>
+  ) : null;
 };
 
 export default OrderSummary;
