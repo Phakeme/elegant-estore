78733493351b281a5023ff0ac0b78d1d10521279
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-10-24 07:52:43 +0200

    SPHelp: Added MarginTop

--- src/Components/SPHelp/index.jsx
+++ src/Components/SPHelp/index.jsx
@@ -8,6 +8,7 @@ import { GrDeliver } from "react-icons/gr";
 import FAQ from "./Accordion";
 import QALink from "./QALink";
 import Styles from "./styles";
+import { MarginTop } from "../";
 
 const SPHelp = ({ qaItems, children }) => {
   const classes = Styles();
@@ -20,23 +21,26 @@ const SPHelp = ({ qaItems, children }) => {
     { label: "Other Enquiries", icon: <FaQuestionCircle /> },
   ];
   return (
-    <Paper className={classes.paper}>
-      <div className={classes.banner}></div>
-      <Grid container spacing={2} style={{ padding: 10 }}>
-        <Grid item xs={12} md={8} style={{ marginTop: "-80px" }}>
-          <div className={classes.gridArea}>
-            {qALinks.map(({ label, icon }, i) => (
-              <QALink key={i} label={label} icon={icon} />
-            ))}
-          </div>
-          <FAQ qaItems={qaItems} />
+    <MarginTop>
+      <Paper className={classes.paper}>
+        <div className={classes.banner}></div>
+        <Grid container spacing={2} style={{ padding: 10 }}>
+          <Grid item xs={12} md={8} className={classes.main}>
+            <Grid container spacing={2} className={classes.boxes}>
+              {qALinks.map(({ label, icon }, i) => (
+                <QALink key={i} label={label} icon={icon} />
+              ))}
+            </Grid>
+            <FAQ qaItems={qaItems} />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <h3 className={classes.label}>DISCOVER ELEGANT</h3>
+            <hr />
+            {children}
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={4}>
-          <h3 className={classes.label}>DISCOVER ELEGANT</h3>
-          {children}
-        </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+    </MarginTop>
   );
 };
 
